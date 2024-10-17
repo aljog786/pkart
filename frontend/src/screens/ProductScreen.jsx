e76ecf2322322009e2436f04d783228c1105a05c
aljog786 <aljog786@gmail.com>
2024-10-17 15:54:39 +0530

    fetching data through redux not axios

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,27 +1,24 @@
-import { useState,useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Row,Col,Image,ListGroup,Card,Button } from "react-bootstrap";
 import Rating from "../components/Rating";
-import axios from "axios";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
-    const [product,setProduct] = useState({});
-
     const {id:productId} = useParams();
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get(`/products/${productId}`)
-            setProduct(data);
-        }
-        fetchProduct();
-    },[productId]);
+    const { data:product,isLoading,error } = useGetProductDetailsQuery(productId);
+
   return (
     <>
     <Link className="btn btn-light my-3" to='/'>
         back
     </Link>
-    <Row>
+    { isLoading ? (
+        <h2>Loading...</h2>
+    ) : error ? (
+        <div>{ error?.data?.message || error.error}</div>
+    ) : (
+         <Row>
         <Col md={6}>
             <Image src={product.image} alt={product.name} fluid />
         </Col>
@@ -69,6 +66,8 @@ const ProductScreen = () => {
             </Card>
         </Col>
     </Row>
+    )}
+    
     </>
   )
 }
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -7,11 +7,17 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             query: () => ({
                 url: PRODUCTS_URL
             }),
-            keepUnusedDataFor: 5
+            keepUnusedDataFor: 5,
+        }),
+            getProductDetails: builder.query({
+                query: (productId) => ({
+                    url: `${PRODUCTS_URL}/${productId}`
+                }),
+                keepUnusedDataFor: 5
+            })
         })
     })
-})
 
 
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery,useGetProductDetailsQuery } = productsApiSlice;
 // path: frontend/src/slices/productSlice.js
