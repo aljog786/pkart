5a3ef92c51d05f5842b09aeb2f985879f3107d18
aljog786 <aljog786@gmail.com>
2024-10-29 14:35:13 +0530

    modified scripts

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
     // any route that is not api will be redirected to index.html
     app.get('*',(req,res) =>
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html')) 
-    )
+    );
 } else {
     app.get('/',(req,res) => { 
         res.send('API running ...');
@@ -52,5 +52,5 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port,() => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 })
